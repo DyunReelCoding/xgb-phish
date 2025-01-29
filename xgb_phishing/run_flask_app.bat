9f20fc86b647f757104957960ed782b6671d9061
Jonriel <iamkirito50@gmail.com>
2025-01-29 12:28:48 +0800

    added batch file

---
+++ xgb_phishing/run_flask_app.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Install necessary dependencies (if not already installed)
+REM python -m pip install -r requirements.txt
+
+REM Run the Flask app (replace with the correct path to your Python script)
+python app.py
+
+REM Deactivate the virtual environment when done
+deactivate
